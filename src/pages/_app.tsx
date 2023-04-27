649488100c2332cfd36a2aa83fd531303a2a9b39
Ahmer Malik <ahmermalik@Ahmers-MacBook-Air.local>
2023-04-27 12:22:17 -0500

    implement react routers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Router>
+      <Component {...pageProps} />
+    </Router>
+  )
 }
